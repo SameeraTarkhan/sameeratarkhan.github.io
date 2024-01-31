27d0c091dcd966ef1e41f88154d40e2f6aa15c66
SameeraTarkhan <sameeratarkhan13@gmail.com>
2024-01-31 11:57:03 +0300

    new update

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,15 +1,36 @@
-// define a function to enable the disabled textbox (Last Name)
-function enableInput() {
-  document.getElementById("lastname").disabled = false;
-}
 // define a function to change the pictures
-function turnOn() {
-  document.getElementById("pic1").src = "/images/5dbf8f6f5b229c461e38f1eab8ed30f9.jpg";
-  document.getElementById("pic2").src = "/images/d4dfe9bb6b59139092241408ec2cc4a2.jpg";
-}
+function change1() {
+    document.getElementById("pic1").src = "/images/men2.jpg";
+  }
+  
+  // define a function to change the pictures back
+  function change2() {
+    document.getElementById("pic1").src = "/images/men1.jpg";
+  }
 
-// define a function to change the pictures back
-function turnOff() {
-  document.getElementById("pic1").src = "/images/adc9dffcaf0582934c7566998f9f5bbd.jpg";
-  document.getElementById("pic2").src = "/images/36aa7302ed45b2252f67889fdafa377b.jpg";
-}
+  function change3() {
+    document.getElementById("pic2").src = "/images/men1.jpg";
+  }
+  
+  // define a function to change the pictures back
+  function change4() {
+    document.getElementById("pic2").src = "/images/men2.jpg";
+  }
+
+  function change5() {
+    document.getElementById("pic3").src = "/images/men4.jpg";
+  }
+  
+  // define a function to change the pictures back
+  function change6() {
+    document.getElementById("pic3").src = "/images/men3.jpg";
+  }
+
+  function change7() {
+    document.getElementById("pic4").src = "/images/men3.jpg";
+  }
+  
+  // define a function to change the pictures back
+  function change8() {
+    document.getElementById("pic4").src = "/images/men4.jpg";
+  }
