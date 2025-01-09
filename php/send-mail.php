1eb99876afb632ebf18800cbf59eb1e42ff629cf
SameeraTarkhan <77339727+SameeraTarkhan@users.noreply.github.com>
2025-01-09 10:21:13 +0300

    1/9

--- php/send-mail.php
+++ php/send-mail.php
@@ -11,16 +11,16 @@ if(isset($_POST["UserSubmit"])) {
     $mail = new PHPMailer(true);
     try {
         $mail->isSMTP();
-        $mail->Host = 'smtp-mail.outlook.com';  // Set the SMTP server to send through
+        $mail->Host = 'smtp.gmail.com';  // Set the SMTP server to send through
         $mail->SMTPAuth = true;
-        $mail->Username = 'admin@thru-tech.com';  // Use environment variables for security
-        $mail->Password = 'Admin2024';  // Use environment variables for security
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
+        $mail->Username = 'thrusameera@gmail.com';  // Use environment variables for security
+        $mail->Password = 'pkhc qefw wvbc tahp';  // Use environment variables for security
+        $mail->SMTPSecure = 'ssl';  // Enable TLS encryption
+        $mail->Port = 465;
 
         // Sender's email
         $mail->setFrom($_POST["UserEmail"]);
-        $mail->addAddress('admin@thru-tech.com');  // Add a recipient
+        $mail->addAddress('thrusameera@gmail.com');  // Add a recipient
 
         // Prepare the body with the message and the user's email
         $messageBody = "<p><strong>User's Email:</strong> " . $_POST["UserEmail"] . "</p>";
